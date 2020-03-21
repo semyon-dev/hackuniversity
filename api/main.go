8fe599f5ddd6f63f92020a0a1df2fe45e274edae
semyon-dev <semyon-dev@protonmail.com>
2020-03-22 01:44:24 +0300

    conflict

--- api/main.go
+++ api/main.go
@@ -91,8 +91,8 @@ func main() {
 	})
 
 	r.GET("/maindata", func(context *gin.Context) {
-		name,dateTimeStart,dateTimeEnd := nameDateTimes(context)
-		fmt.Println(name,dateTimeStart,dateTimeEnd, " values from query")
+		name, dateTimeStart, dateTimeEnd := nameDateTimes(context)
+		fmt.Println(name, dateTimeStart, dateTimeEnd, " values from query")
 
 		min := minValue(name, dateTimeStart, dateTimeEnd)
 		max := maxValue(name, dateTimeStart, dateTimeEnd)
@@ -105,8 +105,6 @@ func main() {
 				"max": max,
 			})
 	})
-
-
 
 	err := r.Run(":5000")
 	if err != nil {
