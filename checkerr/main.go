408836f1ff63205e32abedcd8ea40cd77d29d3d9
Kesha <kesha.tkachenko2017@mail.ru>
2020-03-21 14:18:04 +0300

    refactor

--- checkerr/main.go
+++ checkerr/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gorilla/websocket"
+	"github.com/semyon-dev/hackuniversity/checkerr/DB"
 	. "github.com/semyon-dev/hackuniversity/checkerr/log"
 	"log"
 	"net/http"
@@ -20,60 +21,7 @@ var conn *sql.DB
 var upgrader = websocket.Upgrader{} // use default options
 var Connections []*websocket.Conn
 
-func connect() {
-	var err error
 
-	// language=SQL
-	connStr := "host=localhost port=5432 user=postgres dbname=postgres password=12345678 sslmode=disable"
-
-	conn, err = sql.Open("postgres", connStr)
-	if err != nil {
-		panic(err)
-	}
-
-	_, err = conn.Exec(`CREATE TABLE IF NOT EXISTS errors(
- 					id SERIAL PRIMARY KEY,
- 					dateTime Date,
- 					paramName varchar(20),
- 					paramValue float8,
- 					message text
- 					   
-		)
-`)
-	if err != nil {
-		fmt.Println(err)
-		panic(err)
-	}
-
-}
-
-func insertError(name, message string, paramValue float64) error {
-	_, err := conn.Exec("INSERT INTO errors(dateTime,paramName,paramValue,message) VALUES(now(),$1,$2,$3)", name, paramValue, message)
-	return err
-}
-
-type Criticals struct {
-	Name string  `json:"param"`
-	Min  float64 `json:"min"`
-	Max  float64 `json:"max"`
-}
-
-func getCriticals() map[string]map[string]float64 {
-	rows, err := conn.Query("SELECT paramname,minimum,maximum FROM criticals")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var criticals map[string]map[string]float64
-	var name string
-	var min, max float64
-	for rows.Next() {
-		rows.Scan(&name, &min, &max)
-		criticals[name] = map[string]float64{"min": min, "max": max}
-
-	}
-
-	return criticals
-}
 
 func echo(w http.ResponseWriter, r *http.Request) {
 	c, err := upgrader.Upgrade(w, r, nil)
@@ -111,27 +59,21 @@ func checkCriticalParameters(jsonData []byte) {
 		fmt.Println(err)
 	}
 
-	criticals := getCriticals()
+	criticals := DB.GetCriticals()
 
 	for key, val := range data {
 		if (val < criticals[key]["min"]) && (val > criticals[key]["max"]) {
 			Log.Error(key + " - over normal value")
-			err := insertError(key, "over normal value", val)
+			err := DB.InsertError(key, "over normal value", val)
 			if err != nil {
 				fmt.Println(err)
 			}
 		}
 	}
-
-	//
-	//if data.WATER >= 30 {
-	//	Log.Error("параметр WATER превышает норму")
-	//	fmt.Println("параметр WATER превышает норму")
-	//}
 }
 
 func main() {
-	connect()
+	DB.Connect()
 
 	Logging()
 	flag.Parse()
